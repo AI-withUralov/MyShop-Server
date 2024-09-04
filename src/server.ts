d0ad359a833c228f0dd52192ae2bc75a4645a41e
Sherzodbek <sherzoduralov285@gmail.com>
2024-09-05 00:42:13 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-console.log("Ishladi")
-
 // Architectural pattern: MVC, Dependency Injection, MVP
 // MVC = MODEL VIEW CONTROLLER
-
-
 // Design pattern: Middleware, Decotar
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+//console.log("PORT: ", process.env.PORT);
+
