38339cc8279ff104261b582003036eec59d6dbbe
Sherzodbek <sherzoduralov285@gmail.com>
2024-09-12 16:12:03 +0900

    fix: modify restaurant login and signup process

--- src/models/member-service.ts
+++ src/models/member-service.ts
@@ -2,27 +2,27 @@ import MemberModel from "../schema/Member-model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/errors";
 import { MemberType } from "../libs/enums/member-enum";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
-    // Define a member model that interacts with the database
     private readonly memberModel;
   
-    // Initialize the member model when the service is created
     constructor() {
       this.memberModel = MemberModel; // Assign MemberModel (Mongoose model) to the class variable
     }
   
-    // Asynchronously process the signup for a new member
     public async processSignup(input: MemberInput): Promise<Member> {
-      // Check if a restaurant member already exists in the database
       const exist = await this.memberModel
         .findOne({ memberType: MemberType.RESTAURANT }) 
         .exec(); 
   
-      
       if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
-  
-     
+      
+      console.log("before:", input.memberPassword);
+      const salt = await bcrypt.genSalt();
+      input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+      console.log("after:", input.memberPassword);
+      
       try {
         const result = await this.memberModel.create(input); // Create a new member in the database with the provided input
   
@@ -41,8 +41,11 @@ class MemberService {
         )
         .exec();
         if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
-        
-       const isMatch = input.memberPassword === member.memberPassword;
+        const isMatch = await bcrypt.compare(
+            input.memberPassword,
+            member.memberPassword
+        )
+       //const isMatch = input.memberPassword === member.memberPassword;
        if(!isMatch) {
             throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
        }    
