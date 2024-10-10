3df4325c60adeb42f5e48e37e680cf0821066438
Sherzodbek <sherzoduralov285@gmail.com>
2024-10-11 01:17:47 +0900

    feat: Task T done!

--- src/train.ts
+++ src/train.ts
@@ -243,5 +243,42 @@ function missingNumber(nums: number[]): number {
 }
 
 
-console.log("Yetishmagan son:",missingNumber([0, 1])); // 2
+// console.log("Yetishmagan son:",missingNumber([0, 1])); // 2
 
+/* ------ Task T -------  */
+//Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
+//MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
+
+
+function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+  let mergedArray: number[] = [];
+  let i = 0; // arr1 uchun index
+  let j = 0; // arr2 uchun index
+
+  // Ikkala arrayni birlashtirish
+  while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] < arr2[j]) {
+          mergedArray.push(arr1[i]);
+          i++;
+      } else {
+          mergedArray.push(arr2[j]);
+          j++;
+      }
+  }
+
+  // Qolgan elementlarni qo'shish
+  while (i < arr1.length) {
+      mergedArray.push(arr1[i]);
+      i++;
+  }
+
+  while (j < arr2.length) {
+      mergedArray.push(arr2[j]);
+      j++;
+  }
+
+  return mergedArray;
+}
+
+const Task_T = mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]);
+console.log(Task_T);
