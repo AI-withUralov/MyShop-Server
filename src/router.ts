124cff2c8ed44dc3a8b5b578b01fd6214a496691
Sherzodbek <sherzoduralov285@gmail.com>
2024-10-17 16:31:24 +0900

    feat: develop verify authenticated user credentials

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,14 @@ import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member-controller";
 
+/** Member */
+router.post("/member/signup", memberController.signup);
+router.post("/member/login", memberController.login);
+router.get("/member/detail", memberController.verifyAuth);
 
-router.post("/signup", memberController.signup);
-router.post("/login", memberController.login);
 
+/** Product */
+
+
+/** Order */
 export default router;
