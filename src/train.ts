794e7a6d3158b49e1edfb0767921cddf963e22b4
Sherzodbek <sherzoduralov285@gmail.com>
2024-10-31 22:48:22 +0900

    feat: Task ZB done

--- src/train.ts
+++ src/train.ts
@@ -411,4 +411,16 @@ function sortByAge(arr: Person[]): Person[] {
 
 
 const sortedArray = sortByAge([{ age: 23 }, { age: 21 }, { age: 13 }]);
-console.log(sortedArray);
+//console.log(sortedArray);
+
+/* ------ Task ZB -------  */
+//Shunday function yozing, uni 2 ta number parametri bolsin va berilgan sonlar orasidan random raqam return qilsin
+//MASALAN: randomBetween(30, 50) return 45
+
+function randomBetween(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+const randomNumber = randomBetween(20, 65);
+console.log(randomNumber); 
