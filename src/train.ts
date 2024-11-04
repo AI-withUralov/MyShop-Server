35ef04d92afcb940c96fe5a97d85e516e7bfff43
Sherzodbek <sherzoduralov285@gmail.com>
2024-11-04 14:51:33 +0900

    feat: Task ZC done

--- src/train.ts
+++ src/train.ts
@@ -423,4 +423,24 @@ function randomBetween(min: number, max: number): number {
 
 
 const randomNumber = randomBetween(20, 65);
-console.log(randomNumber); 
+//console.log(randomNumber); 
+
+
+/** TASK ZC */
+
+/**Selisy (°C) shkalasi bo'yicha raqam qabul qilib, uni Ferenhayt (°F) shkalisaga o'zgaritib beradigan function yozing.
+
+MASALAN: celsiusToFahrenheit(0) return 32;
+MASALAN: celsiusToFahrenheit(10) return 50;
+
+Yuqoridagi misolda, 0°C, 32°F'ga teng.
+Yoki 10 gradus Selsiy, 50 Farenhaytga teng.
+
+°C va °F => Tempraturani o'lchashda ishlatiladigan o'lchov birligi. */
+
+function celsiusToFahrenheit(celsius: number): number {
+  return (celsius * 9/5) + 32;
+}
+
+
+console.log("Temperatura:",celsiusToFahrenheit(25)); 
