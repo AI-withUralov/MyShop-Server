fe768a583d39867d94cc6b2c59409136e4bbc421
Sherzodbek <sherzoduralov285@gmail.com>
2024-11-08 19:03:21 +0900

    feat: Task ZD done!

--- src/train.ts
+++ src/train.ts
@@ -443,4 +443,26 @@ function celsiusToFahrenheit(celsius: number): number {
 }
 
 
-console.log("Temperatura:",celsiusToFahrenheit(25)); 
+//console.log("Temperatura:",celsiusToFahrenheit(25)); 
+
+
+/**Shunday function yozing. Bu function o'ziga, parametr sifatida
+birinchi oddiy number, keyin yagona array va uchinchi bo'lib oddiy number
+qabul qilsin. Berilgan birinchi number parametr, arrayning tarkibida indeks bo'yicha hisoblanib,
+shu aniqlangan indeksni uchinchi number parametr bilan alashtirib, natija sifatida
+yangilangan arrayni qaytarsin.
+
+MASALAN: changeNumberInArray(1, [1,3,7,2], 2) return [1,2,7,2];
+
+Yuqoridagi misolda, birinchi raqam bu '1' va arrayning '1'chi indeksi bu 3.
+Bizning function uchinchi berilgan '2' raqamini shu '3' bilan almashtirib,
+yangilangan arrayni qaytarmoqda. */
+
+function changeNumberInArray(firstNumber: number, array: number[], thirdNumber: number): number[] {
+  if (firstNumber >= 0 && firstNumber < array.length) {
+      array[firstNumber] = thirdNumber;
+  }
+  return array;
+}
+
+console.log(changeNumberInArray(1, [1, 3, 7, 2], 2)); 
