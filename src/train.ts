f6895263387eb944d9f799f964b03ad08fb052dd
Sherzodbek <sherzoduralov285@gmail.com>
2024-12-04 12:19:25 +0900

    feat: Task ZM done!

--- src/train.ts
+++ src/train.ts
@@ -593,4 +593,24 @@ function stringToKebab(input: string): string {
 }
 
 // Misol:
-console.log(stringToKebab("This is Jason")); 
+//console.log(stringToKebab("This is Jason")); 
+
+
+// ZM-TASK:
+
+// Shunday function yozing, uni array va number parametri bolsin. Ikkinchi parametrda berilgan raqamli indexgacha arrayni orqasiga ogirib qaytarsin.
+// MASALAN: rotateArray([1, 2, 3, 4, 5, 6], 3) return [5, 6, 1, 2, 3, 4]
+
+function rotateArray(arr: any[], index: number): any[] {
+  if (index < 0 || index >= arr.length) {
+    throw new Error("Index out of bounds");
+  }
+  
+  const part1 = arr.slice(0, index + 1);
+  const part2 = arr.slice(index + 1);
+  
+  return [...part2, ...part1];
+}
+
+const ZM_TASK= rotateArray([1, 2, 3, 4, 5, 6], 3);
+console.log(ZM_TASK); 
